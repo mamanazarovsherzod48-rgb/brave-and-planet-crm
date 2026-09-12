17f9604d24d086fe860773fd8095f296b07df4ed
mamanazarovsherzod48-rgb <mamanazarovsherzod48@gmail.com>
2026-09-12 11:20:27 +0500

    Add files via upload

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = "https://qvtthgoeythyqdpslsqh.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2dHRoZ29leXRoeXFkcHNsc3FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNzc1OTksImV4cCI6MjA1NTc1MzU5OX0.3aLpP7e4j9J7qXg4KjQ8rY1o0PZ7yL4aU1j2l9V8y_c";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2dHRoZ29leXRoeXFkcHNsc3FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3NjYzNDUsImV4cCI6MjEwMjM0MjM0NX0.0xVgaUHGwJCuZVO1gLBn18hiP5cHwqQnLrA46SRi5Ao"; // App.jsx dagi to'liq SUPABASE_ANON_KEY kalitingiz
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
